cc279f5043d82e050e007985b66fc6db9bba67ae
UnlockTool Admin <admin@unlocktool.us>
2026-03-05 09:07:57 +0700

    style: bump CSS version to 5.4 to bust cache for table header fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></noscript>
     <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></noscript>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=5.3">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=5.4">
     @yield('schema')
     @yield('head')
 </head>
